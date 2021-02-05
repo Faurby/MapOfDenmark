8279583d17cbf6c2b7cfc021bc64d9901208b693
Troels Bjerre Lund <trbj@itu.dk>
2021-02-05 11:43:42 +0100

    Add Simple GUI

--- app/src/main/java/bfst21/addressparser/GUI.java
+++ app/src/main/java/bfst21/addressparser/GUI.java
@@ -1,7 +1,31 @@
 package bfst21.addressparser;
 
-public class GUI {
-    public static void main(String[] args) {
-        System.out.println("Not a GUI yet...");
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        var input = new TextField();
+        var output = new TextArea();
+        var pane = new BorderPane();
+        input.setFont(new Font(30));
+        output.setFont(new Font(30));
+        input.setOnAction(e -> {
+            var raw = input.getText();
+            var parsed = Address.parse(raw);
+            output.setText(parsed.toString());
+        });
+        pane.setTop(input);
+        pane.setCenter(output);
+        primaryStage.setScene(new Scene(pane));
+        primaryStage.setTitle("Advanced Address Alignment Advertiser 2.0");
+        primaryStage.show();
     }
 }
