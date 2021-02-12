e0c1d384ea013bfca7a2cf2ca911fa34bed54210
Troels Bjerre Lund <trbj@itu.dk>
2021-02-12 10:12:42 +0100

    Add App.java

---
+++ app/src/main/java/bfst21/vector/App.java
@@ -0,0 +1,13 @@
+package bfst21.vector;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+}
