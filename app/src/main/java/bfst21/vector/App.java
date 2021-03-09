06dd41f245022817b66ff9eac2971e28976cded3
REXKrash <rexkrash@yahoo.dk>
2021-03-09 16:17:37 +0100

    Fixed jar file creation again

--- app/src/main/java/bfst21/vector/App.java
+++ app/src/main/java/bfst21/vector/App.java
@@ -8,7 +8,14 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         var filename = "amager.zip.obj";
+        if (getParameters().getRaw().size() > 0) {
+            filename = getParameters().getRaw().get(0);
+        }
         var model = new Model(filename);
         new View(model, primaryStage);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
