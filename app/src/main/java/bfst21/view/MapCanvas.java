2e7d4da6268773f55bca6bbf4d6f1257acaf9886
Thor Tudal Lauridsen <thtl@itu.dk>
2021-05-14 15:37:26 +0200

    Fix navigation and distance

--- app/src/main/java/bfst21/view/MapCanvas.java
+++ app/src/main/java/bfst21/view/MapCanvas.java
@@ -247,6 +247,15 @@ public class MapCanvas extends Canvas {
                 int exitCount = 0;
                 double weightSum = 0;
 
+                if (edgeList.size() == 1) {
+                    Edge before = edgeList.get(0);
+
+                    before.draw(directedGraph, gc);
+                    distanceSum = before.getDistance() * 1_000.0f;
+                    routeDistance = distanceSum;
+                    currentDirections.add("Follow " + before.getName() + " " + distanceSumToString(distanceSum));
+                }
+                
                 for (int i = 0; i < (edgeList.size() - 1); i++) {
                     Edge before = edgeList.get(i);
                     Edge after = edgeList.get(i + 1);
@@ -560,7 +569,14 @@ public class MapCanvas extends Canvas {
         if (distance >= 1_000.0f) {
             distance /= 1_000.0f;
             String distanceString = "" + distance;
-            return distanceString.substring(0, 3) + " km";
+
+            if (distanceString.contains(".")) {
+                String[] split = distanceString.split("\\.");
+                return split[0] + "." + split[1].charAt(0) + " km";
+
+            } else {
+                return distanceString + " km";
+            }
 
         } else if (distance > 10.0f) {
             distance = (Math.round(distance / 10.0f) * 10.0f);
